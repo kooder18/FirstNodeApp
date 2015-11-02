f152790026db87dc412a953539da56aa40746396
kooder18 <derekkooyman@gmail.com>
2015-11-02 10:15:30 -0700

    added instagram utility just need to provide your own access key

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,23 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+//put your access token from instagram here
+var ACCESS_TOKEN = '';
+
 
 router.get('/dashboard', function(req, res) {
-  res.render('dashboard', {
-		// layout: 'auth_base',
-    // title: 'User Dashboard!',
-    // welcome: 'Welcome to your dashboard!'
-  })
+  var options = {
+		url: 'https://api.instagram.com/v1/users/self/feed?access_token=' + ACCESS_TOKEN,
+	}
+	request.get(options, function(error, response, body){
+		console.log(body)
+	var feed = JSON.parse(body)
+	res.render('dashboard', {
+		feed: feed.data
+	})
 })
+  })
+
 
 router.get('/profile', function(req, res) {
   res.render('profile', {
