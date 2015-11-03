1bca158cf6cb4d78be97cb78af7117f40fa3b4ae
sjkelleyjr <sjkelleyjr@gmail.com>
2015-11-03 11:12:58 -0700

    searching functionality works

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var request = require('request')
 var router = express.Router();
-var ACCESS_TOKEN = 'ACCESSTOKENGOESHERE';
+var ACCESS_TOKEN = '';
 
 router.get('/', function(req, res) {
   res.render('search', {
@@ -17,17 +17,37 @@ router.post('/',function(req,res){
   var form = req.body
   console.log(form.search)
     var options = {
-    url: 'https://api.instagram.com/v1/tags/search?q=' + form.search + '&access_token=' + ACCESS_TOKEN,
+      url:'https://api.instagram.com/v1/tags/'+ form.search + '/media/recent?access_token=' + ACCESS_TOKEN,
+    // url: 'https://api.instagram.com/v1/tags/search?q=' + form.search + '&access_token=' + ACCESS_TOKEN,
   }
   request.get(options,function(error,response,body){
     var feed = JSON.parse(body)
-    console.log(feed)
-    // res.render('searchResult',{
-    //   //instagram stores all the users data in a data object.
-    //   feed: feed.data
-    // })
+    console.log(feed.data)
+    res.render('searchResult',{
+      //instagram stores all the users data in a data object.
+      feed: feed.data
+    })
   })
 })
+
+
+router.post('/searchResult',function(req,res){
+  var form = req.body
+  console.log(form.search)
+    var options = {
+      url:'https://api.instagram.com/v1/tags/'+ form.search + '/media/recent?access_token=' + ACCESS_TOKEN,
+    // url: 'https://api.instagram.com/v1/tags/search?q=' + form.search + '&access_token=' + ACCESS_TOKEN,
+  }
+  request.get(options,function(error,response,body){
+    var feed = JSON.parse(body)
+    console.log(feed.data)
+    res.render('searchResult',{
+      //instagram stores all the users data in a data object.
+      feed: feed.data
+    })
+  })
+})
+
 
 router.get('/searchResult', function(req, res) {
   res.render('searchResult', {
